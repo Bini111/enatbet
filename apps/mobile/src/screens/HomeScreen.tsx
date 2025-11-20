f7b7980d8ef8e53cf5f56c1591f779a1a833c29e
Bini <biniamkelemework@gmail.com>
2025-11-19 21:25:26 -0500

    Fix HomeScreen navigation with useNavigation hook

--- apps/mobile/src/screens/HomeScreen.tsx
+++ apps/mobile/src/screens/HomeScreen.tsx
@@ -6,14 +6,13 @@ import {
   ScrollView,
   TouchableOpacity,
 } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/types";
 
-type HomeScreenProps = {
-  navigation: NativeStackNavigationProp<RootStackParamList, "Home">;
-};
+export default function HomeScreen() {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
-export default function HomeScreen({ navigation }: HomeScreenProps) {
   return (
     <ScrollView style={styles.container} bounces={false}>
       {/* Hero Section */}
